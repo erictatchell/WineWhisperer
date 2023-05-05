bf55e198279340950c60b32c32568478f5a8d68a
Eric Tatchell <erictatch@gmail.com>
2023-05-05 10:28:45 -0700

    Converted to /pages router. /app router limited fn Google authentication fully complete Resized icons for mobile MongoDB authentication not functional

---
+++ pages/main/settings.tsx
@@ -0,0 +1,21 @@
+import { getSession } from 'next-auth/react';
+import './globals.css'
+
+export default function Settings() {
+    return (
+        <h1>Settings page</h1>
+    )
+}
+export const getServerSideProps = async (context: any) => {
+    const session = await getSession(context);
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/'
+            }
+        }
+    }
+    return {
+        props: { session }
+    }
+}
