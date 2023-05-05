40e1640223f3bf176654ae53f5304c21885b50bb
noorksangha <113377440+noorksangha@users.noreply.github.com>
2023-05-05 10:23:06 -0700

    Added mock of top picks.

--- src/app/main/toppicks/page.tsx
+++ src/app/main/toppicks/page.tsx
@@ -1,5 +1,18 @@
+import useCSVData from './useCSVData';
+
 export default function TopPicks() {
-    return (
-        <h1>Top Picks page</h1>
-    )
+  const data = useCSVData('/Users/noorsangha/2800-202310-BBY29-5/winedata1.csv');
+
+  return (
+    <div>
+      <h1>Top Picks page</h1>
+      <ul>
+        {data.map((row, index) => (
+          <li key={index}>
+            {row.fixedacidity} - {row.volatile} - {row.acidity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
