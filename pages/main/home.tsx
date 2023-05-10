b625aa0fc08ba0940db71a8d3075470788c3eebb
Eric Tatchell <erictatch@gmail.com>
2023-05-09 21:31:42 -0700

    created generic Wine object, moved ai.js to api

--- pages/main/home.tsx
+++ pages/main/home.tsx
@@ -1,10 +1,9 @@
-import { getSession } from "next-auth/react";
 import { useState } from 'react';
-import { getWineRecommendations } from '../../components/ai';
 
 export default function Home() {
     const [selectedPrompt, setSelectedPrompt] = useState('list');
     const [description, setDescription] = useState('');
+    const [wineResults, setWineResults] = useState<Wine[]>([]);
 
     const handleDropdownChange = (event: any) => {
         setSelectedPrompt(event.target.value);
@@ -14,11 +13,24 @@ export default function Home() {
         setDescription(event.target.value);
     };
 
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+
+        const res = await fetch('/api/recommendations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ selectedPrompt, description }),
+        });
+
+        const wineData: Wine[] = await res.json();
+        setWineResults(wineData);
+    };
+
     return (
         <div>
-            <h1>Home page</h1>
-            {/* ugly ass form, can replace with a nice tailwind form but KEEP FN!*/}
-            <form>
+            <form onSubmit={handleSubmit}>
                 <select value={selectedPrompt} onChange={handleDropdownChange}>
                     <option value="list">List of 5 wines</option>
                     <option value="single">Single wine</option>
@@ -27,21 +39,11 @@ export default function Home() {
                 <textarea value={description} onChange={handleInputChange} />
                 <button type="submit">Get recommendations</button>
             </form>
+            <div>
+                {wineResults.map((wine) => (
+                    <div key={wine._id}>{wine.title}</div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
-
-// export const getServerSideProps = async (context: any) => {
-//     const session = await getSession(context);
-//     if (!session) {
-//         return {
-//             redirect: {
-//                 destination: '/',
-//                 permanent: false,
-//             },
-//         };
-//     }
-//     return {
-//         props: { session },
-//     };
-// }
