d6ac2b9495b893ed5dbcde2465bb79728e959436
noorksangha <113377440+noorksangha@users.noreply.github.com>
2023-05-10 10:07:16 -0700

    Connected wine dataset to toppicks

--- pages/main/toppicks.tsx
+++ pages/main/toppicks.tsx
@@ -1,20 +1,57 @@
+import { GetServerSideProps } from 'next';
 import { getSession } from "next-auth/react";
+import clientPromise from '../../lib/mongodb';
 
-export default function TopPicks() {
+interface Wine {
+  _id: string;
+  id: number;
+  country: string;
+  description: string;
+  designation: string;
+  points: number;
+  price: number;
+  province: string;
+  region_1: string;
+  region_2: string;
+  taster_name: string;
+  taster_twitter_handle: string;
+  title: string;
+  variety: string;
+  winery: string;
+}
+
+interface TopPicksProps {
+  wines: Wine[];
+}
+
+export default function TopPicks({ wines }: TopPicksProps) {
     return (
-        <h1>Top Picks page</h1>
+        <div>
+            <h1>Top Picks page</h1>
+            {wines.map((wine: Wine, index: number) => (
+                <div key={index}>
+                    <h2>{wine.title}</h2>
+                    <p>{wine.description}</p>
+                    {/* You can add more fields here */}
+                </div>
+            ))}
+        </div>
     )
 }
-export const getServerSideProps = async (context: any) => {
-    const session = await getSession(context);
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/'
-            }
-        }
-    }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const client = await clientPromise;
+    const db = client.db('Wine1'); // your database name
+    const wines = await db
+        .collection('wset')
+        .find({})
+        .limit(10)
+        .toArray();
+
     return {
-        props: { session }
-    }
-}
+        props: {
+            wines: JSON.parse(JSON.stringify(wines)),
+        },
+    };
+};
+
