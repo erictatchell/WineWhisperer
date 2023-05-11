4451babd2d0b9326e7f208d622265bb4fadbff1c
Eric Tatchell <erictatch@gmail.com>
2023-05-11 14:08:38 -0700

    removing jwt and session callbacks session wasnt creating

--- pages/main/profile.tsx
+++ pages/main/profile.tsx
@@ -44,8 +44,8 @@ export default function Profile() {
 
       <div className="mt-0">
         <h3 className="text-l font-bold">User ID:</h3>
-        {/* <h1 className="text-l">{user ? user._id.toString().slice(0, 8) : null}</h1> */}
-        <h1 className="text-m">1234567878</h1>
+        {/** Insert User ID here! */}
+        <h1 className="text-m"></h1>
       </div>
 
       {/* Sign out button */}
