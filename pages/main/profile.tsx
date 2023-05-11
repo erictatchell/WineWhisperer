e62dc38e2496810f8c6ee0f9e728d8d0596e8f89
Brendan-Doy1e <brendan-doyle@hotmail.com>
2023-05-11 11:23:16 -0700

    Changed sizing of page elements

--- pages/main/profile.tsx
+++ pages/main/profile.tsx
@@ -7,6 +7,9 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 export default function Profile() {
+  const { data: session } = useSession()
+  const user = session ? session.user : null;
+
   return (
     // entire page
     <div className="mt-6 mx-8">
@@ -19,35 +22,36 @@ export default function Profile() {
           <button className="absolute right-0 bottom-0 p-1 rounded-full text-xs drop-shadow-xl mt-3 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-small   text-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Change</button>
         </div>
         {/* Settings button */}
-        <button className="p-2 drop-shadow-xl text-xl mt-3 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-medium rounded-lg px-5 py-3.5  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Settings</button>
+        <button className="p-2 drop-shadow-xl text-l mt-3 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-medium rounded-lg px-5 py-3.5  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Settings</button>
       </div>
 
       {/* User info */}
       <div className="mt-4">
-        <h3 className="text-lg font-bold">Username</h3>
+        <h3 className="text-l font-bold">Username</h3>
         <div className="flex items-top justify-between">
-          <h1 className="text-l">Brendan Doyle</h1>
-          <button className="p-2 drop-shadow-xl text-medium mt-3 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-medium rounded-lg px-2 py-2  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Edit</button>
+          <h1 className="text-m">{user ? user.name : 'No name'}</h1>
+          <button className="p-2 drop-shadow-xl text-medium mt-0 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-small rounded-lg px-1 py-1  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Edit</button>
         </div>
       </div>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-bold">Email</h3>
+      <div className="mt-0">
+        <h3 className="text-l font-bold">Email</h3>
         <div className="flex items-top justify-between">
-          <h2 className="text-l font-light">Brendan@email.com</h2>
-          <button className=" drop-shadow-xl text-medium mt-3 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-medium rounded-lg px-2 py-2  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Edit</button>
+        <h1 className="text-m">{user ? user.email : 'No email'}</h1>
+          <button className="p-2 drop-shadow-xl text-medium mt-0 text-black bg-dijon hover:bg-[#F8DE7F] focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-small rounded-lg px-1 py-1  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Edit</button>
         </div>
       </div>
 
-      <div className="mt-4">
-        <h3 className="text-lg font-bold">User ID:</h3>
-        <h3 className="text-lg font-light items-top">12345</h3>
+      <div className="mt-0">
+        <h3 className="text-l font-bold">User ID:</h3>
+        {/* <h1 className="text-l">{user ? user._id.toString().slice(0, 8) : null}</h1> */}
+        <h1 className="text-m">1234567878</h1>
       </div>
 
       {/* Sign out button */}
       <div className="flex justify-end mt-4">
-        <button className="drop-shadow-xl text-medium mt-3 text-black bg-dijon hover:bg-[#bb2b43] focus:ring-4 focus:outline-none focus:ring-[#bb2b43]/50 font-medium rounded-lg px-2 py-2  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Sign Out</button>
+        <button onClick={() => (signOut())} className="p-2 drop-shadow-xl text-medium mt-3 text-black bg-dijon hover:bg-[#bb2b43] focus:ring-4 focus:outline-none focus:ring-[#bb2b43]/50 font-medium rounded-lg px-2 py-2  text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">Sign Out</button>
       </div>
     </div>
   );
-};
+}
