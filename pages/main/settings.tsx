95d2710469fbdb87bb66a50ba4de5985124963a1
Brendan-Doy1e <brendan-doyle@hotmail.com>
2023-05-11 12:53:56 -0700

    Working on profile pic

--- pages/main/settings.tsx
+++ pages/main/settings.tsx
@@ -5,16 +5,16 @@ export default function Settings() {
         <h1>Settings page</h1>
     )
 }
-export const getServerSideProps = async (context: any) => {
-    const session = await getSession(context);
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/'
-            }
-        }
-    }
-    return {
-        props: { session }
-    }
-}
+// export const getServerSideProps = async (context: any) => {
+//     const session = await getSession(context);
+//     if (!session) {
+//         return {
+//             redirect: {
+//                 destination: '/'
+//             }
+//         }
+//     }
+//     return {
+//         props: { session }
+//     }
+// }
