743d52aa2e2ecb8c83953bf8948b297c37767a1e
victoramaim <vcarvalhodiasamaimva@my.bcit.ca>
2023-05-15 11:15:05 -0700

    Created the fake 404

--- pages/main/aboutus.tsx
+++ pages/main/aboutus.tsx
@@ -1,4 +1,5 @@
 import { getSession } from 'next-auth/react';
+import Link from 'next/link';
 
 export default function aboutUs() {
     return (
@@ -7,9 +8,11 @@ export default function aboutUs() {
             <div className='grid max-w-sm ml-6 mr-6 mb-6 border-brendan rounded-lg shadow dark:bg-brendan/90 dark:border-gray-700 sm:max-w-full'>
                 <p>Our innovative team at BBY29 is proudly developing WineWhisperer, an AI-driven platform designed to empower farmers, vintners, and small businesses in the pursuit of sustainable wine making. Our mission is to connect eco-conscious wine enthusiasts with an exquisite selection of environmentally friendly and delectable wines. By harnessing the power of AI, WineWhisperer is revolutionizing the way we discover, enjoy, and share sustainable wines.</p>
             </div>
+            <Link href="/main/404">
             <div className='grid max-w-sm ml-6 mr-6 mb-6 border-brendan rounded-lg shadow dark:bg-brendan/90 dark:border-gray-700 sm:max-w-full'>
-                <p>Consisting of four exceptional students - Brendan Doyle, Eric Tatchell, Noor Sangha, and Victor Vasconcellos, BBY-29 is a highly motivated and cohesive group from the British Columbia Institute of Technology. We share a passion for computer programs and aspire to make a positive impact on society through our knowledge and hard work.</p>
+                Consisting of four exceptional students - Brendan Doyle, Eric Tatchell, Noor Sangha, and Victor Vasconcellos, BBY-29 is a highly motivated and cohesive group from the British Columbia Institute of Technology. We share a passion for computer programs and aspire to make a positive impact on society through our knowledge and hard work.
             </div>
+            </Link>
         </div>
     )
 }
