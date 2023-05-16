8d2fe96e3432ee0172efd698f79dde56d797cfec
Brendan-Doy1e <brendan-doyle@hotmail.com>
2023-05-16 14:21:52 -0700

    uncommented session verification

--- pages/main/aboutus.tsx
+++ pages/main/aboutus.tsx
@@ -56,22 +56,22 @@ export default function aboutUs() {
 };
 
 // Checking session and redirecting if not logged in
-// export async function getServerSideProps(context: any) {
-//   const session = await getSession(context);
-//   const userEmail = session && session.user ? session.user.email : null;
-//   if (userEmail) {
-//     const client = await clientPromise;
-//     const db = client.db();
-//     const userExtra = await db.collection("userExtras").findOne({ email: userEmail });
-//     if (userExtra) {
-//       return {
-//         props: {
-//           userId: userExtra.id,
-//         },
-//       };
-//     }
-//   }
-//   return {
-//     props: {},
-//   };
-// }
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+  const userEmail = session && session.user ? session.user.email : null;
+  if (userEmail) {
+    const client = await clientPromise;
+    const db = client.db();
+    const userExtra = await db.collection("userExtras").findOne({ email: userEmail });
+    if (userExtra) {
+      return {
+        props: {
+          userId: userExtra.id,
+        },
+      };
+    }
+  }
+  return {
+    props: {},
+  };
+}
