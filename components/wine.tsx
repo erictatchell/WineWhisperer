b949eb7f41597128c627b2f79592edaf69fc6302
Eric Tatchell <erictatch@gmail.com>
2023-05-17 14:29:40 -0700

    created new files, have to pull

---
+++ components/wine.tsx
@@ -0,0 +1,17 @@
+interface Wine {
+    _id: string;
+    id: number;
+    country: string;
+    description: string;
+    designation: string;
+    points: number;
+    price: number;
+    province: string;
+    region_1: string;
+    region_2: string;
+    taster_name: string;
+    taster_twitter_handle: string;
+    title: string;
+    variety: string;
+    winery: string;
+}
