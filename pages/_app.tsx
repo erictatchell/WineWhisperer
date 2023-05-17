f7a7ffa044835cf0dba6c9a22f5cdef73727cebc
Eric Tatchell <erictatch@gmail.com>
2023-05-17 11:47:36 -0700

    moved transition to inside header/footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,37 +35,37 @@ export default function App({
 }: AppProps) {
   const router = useRouter();
   return (
-    <AnimatePresence mode='wait'>
-      <motion.div
-        key={router.route}
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        transition={{
-          duration: 0.75,
-        }}
-        variants={{
-          initialState: {
-            opacity: 0,
-            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-          },
-          animateState: {
-            opacity: 1,
-            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-          },
-          exitState: {
-            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-          },
-        }}
-      >
-        <SessionProvider session={session}>
-          <Layout>
 
+    <SessionProvider session={session}>
+      <Layout>
+        <AnimatePresence mode='wait'>
+          <motion.div
+            key={router.route}
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            transition={{
+              duration: 0.75,
+            }}
+            variants={{
+              initialState: {
+                opacity: 0,
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              },
+              animateState: {
+                opacity: 1,
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              },
+              exitState: {
+                clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+              },
+            }}
+          >
             <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </SessionProvider>
 
-          </Layout>
-        </SessionProvider>
-      </motion.div>
-    </AnimatePresence>
   )
 }
