6972aaeaef17c63ea75d108e67e05fa4a50e12da
noorksangha <113377440+noorksangha@users.noreply.github.com>
2023-05-17 18:51:51 -0700

    refactored the saved button logic for eco page.

--- pages/main/eco.tsx
+++ pages/main/eco.tsx
@@ -8,6 +8,7 @@ import { IconButton, ThemeProvider, createTheme } from '@mui/material';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { FaLeaf } from 'react-icons/fa';
 import WineCard from '../../components/winecard';
+import { useSession } from 'next-auth/react';
 
 
 // Defining a TypeScript interface for the structure of a wine object
@@ -54,12 +55,35 @@ const theme = createTheme({
 /** TODO */
 export default function Eco({ ecowines }: EcoProps) {
     const router = useRouter();
+        const { data: session } = useSession();
+    const user = session ? session.user : null;
 
 
 
-    function handleWineClick(wine: Wine) {
-        localStorage.setItem('WINE' + wine._id, JSON.stringify(wine));
-        router.push(`/wine/${wine._id}`);
+    
+
+    async function saveWineId(wine: Wine) {
+        try {
+            if (user) {
+                const res = await fetch('/api/wine/saveWine', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ wineId: wine._id, email: user.email }),
+                });
+
+                if (res.ok) {
+                    console.log('Wine saved successfully');
+                } else {
+                    console.log('Failed to save wine');
+                }
+            } else {
+                console.log('User is not logged in');
+            }
+        } catch (error) {
+            console.log('An error occurred while trying to save the wine', error);
+        }
     }
 
 
