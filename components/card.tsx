3c5b4ef4f4cdd5a34112edef44c198d7c6942fa8
Brendan-Doy1e <brendan-doyle@hotmail.com>
2023-05-24 12:08:06 -0700

    changed styling on the settings, profile, and aboutus page

--- components/card.tsx
+++ components/card.tsx
@@ -9,7 +9,7 @@ type CardProps = {
 
 const Card: FC<CardProps> = ({ title, icon, isOn, toggleSwitch }) => {
   return (
-    <div className="space-y-8 p-2 drop-shadow-xl text-xl mb-8 mt-0 text-black bg-dijon focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-small rounded-lg px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2">
+    <div className="space-y-8 p-2 drop-shadow-xl text-xl mb-8 mt-0 text-black bg-dijon/70 focus:ring-4 focus:outline-none focus:ring-[#F8DE7F]/50 font-small rounded-lg px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2">
 
       <div className="flex items-center justify-between w-full">
 
@@ -22,10 +22,9 @@ const Card: FC<CardProps> = ({ title, icon, isOn, toggleSwitch }) => {
         </div>
 
         <div>
-          <label className="relative inline-flex items-center cursor-pointer">
+          <label className="relative inline-flex items-center cursor-pointe">
             <input type="checkbox" checked={isOn} onChange={toggleSwitch} className="sr-only peer"/>
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 
-            peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer 
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer 
             dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] 
             after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
             after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600">
